77c0e4855e552a1c09d3e83ec95475ccfc23b471
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2018-03-22 22:29:10 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jmolloy-osdev-tutorial
 Source code from http://www.jamesmolloy.co.uk/tutorial_html
+
+Tested on Ubuntu 16.04 x86_64
+
+
+./update_image <folder name>
---
+++ update_image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CURRENT=`pwd`
+TARGET=$1
+
+cd $TARGET/src
+make
+cp kernel $CURRENT
+make clean
+cd $CURRENT
+
+sudo /sbin/losetup /dev/loop0 floppy.img
+mkdir mnt
+sudo mount /dev/loop0 mnt
+sudo cp kernel mnt/kernel
+sudo cp initrd.img mnt/initrd
+sudo umount /dev/loop0
+rmdir mnt
+sudo /sbin/losetup -d /dev/loop0
